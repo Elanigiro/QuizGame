5dfa47c8633c8a2bb4dd0368db9c244c9d1d2c81
Elanigiro <emilio.gasbarro@outlook.com>
2022-11-15 17:43:51 +0100

    organized imports

--- src/main/java/fictional/quizfinal/controller/ServerSideRESTController.java
+++ src/main/java/fictional/quizfinal/controller/ServerSideRESTController.java
@@ -7,11 +7,8 @@ import java.util.List;
 import java.util.Map;
 
 import javax.validation.Valid;
-import javax.validation.constraints.AssertFalse;
-import javax.validation.constraints.AssertTrue;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,7 +21,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
